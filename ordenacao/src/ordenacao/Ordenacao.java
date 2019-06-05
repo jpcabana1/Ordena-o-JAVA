2952c56a9f43086ebeccdd7efa912b727147818c
jpcabana1 <jpcabana1@gmail.com>
2019-06-05 16:45:37 -0300

    +1

--- ordenacao/src/ordenacao/Ordenacao.java
+++ ordenacao/src/ordenacao/Ordenacao.java
@@ -4,22 +4,23 @@ public class Ordenacao {
 
     public static void main(String[] args) {
         MetodosOrdena m = new MetodosOrdena();
-        int tam = 5;
+        int tam = 6;
         int vetor[] = new int[tam];
         vetor[0] = 15;
         vetor[1] = 5;
         vetor[2] = 34;
         vetor[3] = 15;
         vetor[4] = 10;
+        vetor[4] = 1;
         //vetor[5] = 1;
         //vetor[6] = 4;
         //vetor[7] = 90;
         //vetor[8] = 75;
         //vetor[9] = 18;
         
-        
-        m.insertionSort(vetor, tam);
-        
+        m.printa(vetor, tam);
+        m.quickSort(vetor, 0, tam/2);
+        m.printa(vetor, tam);
         
         
         
